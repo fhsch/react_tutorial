1772edf6841129207d68a1bcacfb57ba594c9905
fhsch <felix.graeffshagen@gmail.com>
2023-03-01 10:34:32 +0100

    props example

---
+++ 02_01_props/index.js
@@ -0,0 +1,53 @@
+function Contact(props) {		
+	return (
+        <div className="contact-card">
+            <img src={props.img} />
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src={"./images/phone-icon.png"} />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{props.email}</p>
+            </div>
+        </div>       
+    )			
+}
+
+// All in here because fuck creating a react app every time or working on scrimba
+function App() {		
+	return (
+        <div className="contacts">
+            <Contact 
+                img="./images/mr-whiskerson.png"
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact 
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            /> 
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            /> 
+        </div>
+    )
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("root")
+)
